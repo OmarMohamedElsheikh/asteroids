dd97533ce15c6dc28099cbd479cffdb8682fd072
Omar Elshaikh <omarmohammedomarali@gmail.com>
2025-09-25 06:37:45 +0300

    creating the background.

--- main.py
+++ main.py
@@ -4,10 +4,19 @@ import pygame
 
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen hight: {SCREEN_HEIGHT}")
-    
+    pygame.init()
+
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    while(True):
+        screen.fill("black")
+
+
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
